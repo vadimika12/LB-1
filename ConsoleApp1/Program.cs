bf322345899249128390a583b2c5b2f608ff09f9
vadimika12 <vadimanton3@gmail.com>
2023-10-26 09:12:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FunctionCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение аргумента x: ");
+            double x = double.Parse(Console.ReadLine());
+
+            double y = 1 / Math.Pow(1+x,2);
+
+            Console.WriteLine("Значение функции y = " + y);
+        }
+    }
+}
